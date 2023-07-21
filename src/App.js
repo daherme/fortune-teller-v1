2957565095e06580e343bc5eb3cb43d9a05d2322
daherme <daher.maryjane@gmail.com>
2023-07-21 17:39:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Video from "./videos/video1.mp4";
+import "./style.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <video autoPlay muted loop className="myVideo">
+        <source src={Video} type="video/mp4"></source>
+      </video>
+      <div className="content">
+        <h1>FORTUNE TELLER</h1>
+        <div className="adviceContainer">
+          <p className="advice">{advice}</p>
+          <button className="button" onClick={getAdvice}>
+            Get some advice..
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
